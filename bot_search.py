5abf8069945e815b30cf6d5093a8891700d52a93
Luan Carminatti <luancarminatt@gmail.com>
2023-11-07 23:20:25 -0300

    criei uma lista para path_to_profile e alterei nome das instancias

--- bot_search.py
+++ bot_search.py
@@ -28,43 +28,45 @@ class edge_bot():
         search_field.send_keys(text)
 
     def interaction_text(self):
-        time.sleep(2)
+        time.sleep(0.6)
         search_field = self.driver.find_element(By.XPATH, "//*[@id='sb_form_q']")
-        time.sleep(3)
+        time.sleep(0.7)
         search_field.send_keys(Keys.BACK_SPACE)
-        time.sleep(4)
+        time.sleep(0.8)
         search_field.send_keys(Keys.ENTER)
 
 # get_api_text = 'É uma ferramenta gratuita que permite gerar frases e citações escolhidas ao acaso de um vasto banco de dados.Quer fazer um post ou um tweet, mas está sem ideias? Chegou ao sítio certo. No mil-frases criamos o gerador de frases aleatorias para ser facil gerar uma frase ao acaso e ter sempre bons resultados. Basta escolher a categoria pretendida e poderá gerar tantas frases quanto quiser. Para utilizar esta ferramenta, basta clicar no botão "Obter nova frase Aleatória" e uma nova citação será exibida na tela. O Mil-Frases também oferece a opção de gerar citações de autores específicos ou frases sobre temas específicos, bastando selecionar as opções desejadas antes de clicar no botão de geração de frases. Além disso, o Mil-Frases permite que as frases geradas sejam compartilhadas nas redes sociais ou baixadas em formato de imagem.'
 
 # configs
-path_to_profile = "C:\\Users\\workcarminatti\\AppData\\Local\\Microsoft\\Edge\\User Data1\\"
-path_to_profile2 = "C:\\Users\\workcarminatti\\AppData\\Local\\Microsoft\\Edge\\User Data2\\"
+path_to_profile = [
+                    "C:\\Users\\workcarminatti\\AppData\\Local\\Microsoft\\Edge\\User Data1\\",
+                    "C:\\Users\\workcarminatti\\AppData\\Local\\Microsoft\\Edge\\User Data2\\"
+                   ]
 path_to_webdriver = "C:\\webdrivers\\msedgedriver.exe"  # Substitua pelo caminho correto do driver
 
 text = 'aauiaoueaoieua teste'
 
 # criando uma nova instancia
-new_bot = edge_bot(path_to_profile, path_to_webdriver)
+jarbinhas = edge_bot(path_to_profile[0], path_to_webdriver)
 
 # print('acessar site')
-new_bot.open_bing()
-time.sleep(3)
+jarbinhas.open_bing()
+time.sleep(0.4)
 
 # print('digitar texto no campo')
-new_bot.write_text(text)
-time.sleep(2)
+jarbinhas.write_text(text)
+time.sleep(0.6)
 
 
 
-new_sesion = edge_bot(path_to_profile2, path_to_webdriver)
-new_sesion.open_bing()
-time.sleep(3)
-new_sesion.write_text(text)
+idgato = edge_bot(path_to_profile[1], path_to_webdriver)
+idgato.open_bing()
+time.sleep(0.5)
+idgato.write_text(text)
 
 while True:
-    new_bot.interaction_text()
-    new_sesion.interaction_text()
+    jarbinhas.interaction_text()
+    idgato.interaction_text()
 
 
 
